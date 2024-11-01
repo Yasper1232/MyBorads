251a656006498b5493c80948498e81edf543fb32
Kacper <ciaparino123@gmail.com>
2024-11-01 19:34:29 +0100

    seedowanie encji

---
+++ MyBorads/Migrations/20241101183144_TagDataSeed.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MyBorads.Migrations
+{
+    /// <inheritdoc />
+    public partial class TagDataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Tags",
+                columns: new[] { "Id", "Category", "Value" },
+                values: new object[,]
+                {
+                    { 1, null, "Web" },
+                    { 2, null, "UI" },
+                    { 3, null, "Desktop" },
+                    { 4, null, "API" },
+                    { 5, null, "Service" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
--- MyBorads/Program.cs
+++ MyBorads/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MyBorads.Entities;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -20,13 +21,40 @@ if (app.Environment.IsDevelopment())
 }
 //jesli nie ma bazy danych lub sa zalegle migracje to taka baza zostanie utworzona tym kodem :
 using var scope = app.Services.CreateScope();
-var dbContext = scope.ServiceProvider.GetService<DbContext>();
+var dbContext = scope.ServiceProvider.GetService<MyBoardsContext>();
 
 var pendingMigrations = dbContext.Database.GetPendingMigrations();
 
 if (pendingMigrations.Any())
 {
     dbContext.Database.Migrate();
+}
+
+var users = dbContext.Users.ToList();
+if (!users.Any())
+{
+    var user1 = new User()
+    {
+        Email = "user1@test.com",
+        FullName = "User one",
+        Adress = new Address()
+        {
+            City = "Warszawa",
+            Street = "Szeroka"
+        }
+    };
+    var user2 = new User()
+    {
+        Email = "user2@test.com",
+        FullName = "User two",
+        Adress = new Address()
+        {
+            City = "Krakow",
+            Street = "Dluga"
+        }
+    };
+    dbContext.Users.AddRange(user1, user2);
+    dbContext.SaveChanges();
 }
 
 //Metoda GetPendingMigrations() jest czêœci¹ API migracji Entity Framework Core. S³u¿y do uzyskania listy migracji, //które zosta³y utworzone, ale jeszcze nie zosta³y zastosowane w bazie danych.
